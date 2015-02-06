b1723eed0e2b3c2fb4b5b9dc60612918ca681254
Giuseppe Cerati <giuseppe.cerati@cern.ch>
2015-02-06 15:04:41 +0100

    add met selection

---
+++ MetSelections.cc
@@ -0,0 +1,38 @@
+#include "MetSelections.h"
+#include "Math/VectorUtil.h"
+
+using namespace tas;
+
+metStruct trackerMET(float deltaZCut, const std::vector<LorentzVector>* jets) {
+
+  if ( vtxs_sumpt().empty() ) return metStruct();
+  double pX(0), pY(0);
+  
+  for (unsigned int i=0; i<pfcands_particleId().size(); ++i){
+    if ( pfcands_charge().at(i)==0 ) continue;
+    if ( jets ){
+      bool matched = false;
+      for ( std::vector<LorentzVector>::const_iterator jet = jets->begin(); jet != jets->end(); ++jet )
+    	if ( fabs(ROOT::Math::VectorUtil::DeltaR(pfcands_p4().at(i),*jet))<0.5 ) matched=true;
+      if (matched) continue;
+    }
+    
+    if ( fabs(pfcands_dz().at(i)) > deltaZCut) continue;
+    
+    pX -= pfcands_p4().at(i).px();
+    pY -= pfcands_p4().at(i).py();
+  }
+  
+  if (jets){
+    for ( std::vector<LorentzVector>::const_iterator jet = jets->begin(); jet != jets->end(); ++jet ){
+      pX -= jet->px();
+      pY -= jet->py();
+    }
+  }
+  metStruct met;
+  met.met     = sqrt(pX * pX + pY * pY);
+  met.metphi  = atan2(pY, pX);
+  met.metx = pX;
+  met.mety = pY;
+  return met;
+}
---
+++ MetSelections.h
@@ -0,0 +1,17 @@
+#ifndef METSELECTIONS_H
+#define METSELECTIONS_H
+
+#include "CMS3.h"
+
+struct metStruct{
+  metStruct() : met(-999.), metphi(-999.), metx(-999.), mety(-999.), sumet(-999.)  {}
+  float met;
+  float metphi;
+  float metx;
+  float mety;
+  float sumet;
+};
+
+metStruct trackerMET(float deltaZCut = 0.2, const std::vector<LorentzVector>* jets = 0);
+
+#endif
