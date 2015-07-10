2da9da3ef3e8b606f30b67a4b0ec2568e86e62e8
isuarez <isuarez@cern.ch>
2015-07-10 15:14:47 -0700

    EventSelections with DCS checking

---
+++ EventSelections.cc
@@ -0,0 +1,12 @@
+#include "EventSelections.h"
+
+using namespace tas;
+
+bool haveFunctionalDCS() {
+    // defined in https://cmssdt.cern.ch/SDT/doxygen/CMSSW_6_1_1/doc/html/d1/d05/DcsStatus_8h_source.html#l00084. Ignore CASTOR and ZDC.
+    std::vector<int> detectors = { 0, 1, 2, 3, 5, 6, 7, 8, 9, 12, 13, 14, 15, 16, 17, 24, 25, 26, 27, 28, 29, 30, 31 };
+    for(unsigned int iDet = 0; iDet < detectors.size(); iDet++) {
+        if( ! ( evt_detectorStatus() & (1 << detectors[iDet]  ) ) ) return false;
+    }
+    return true;
+}
---
+++ EventSelections.h
@@ -0,0 +1,8 @@
+#ifndef EVENTSELECTIONS_H
+#define EVENTSELECTIONS_H
+
+#include "CMS3.h"
+
+bool haveFunctionalDCS();
+
+#endif
