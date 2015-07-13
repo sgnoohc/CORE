83e361ce18a0dee1a897bf7aa7704baf703a5e28
cwelke <cwelke@cern.ch>
2015-07-13 04:18:33 -0700

    updated os selections

--- OSSelections.cc
+++ OSSelections.cc
@@ -14,8 +14,18 @@ bool overlapMuon_ZMET_v1( int index , float ptcut ){
     float dr = ROOT::Math::VectorUtil::DeltaR( cms3.els_p4().at(index) , cms3.mus_p4().at(muind) );    
     if( dr > 0.05                                     ) continue;
     if( cms3.mus_p4().at(muind).pt() < ptcut          ) continue;
-	// if( !muonID( muind, ZMET_loose_v1 )      ) continue;
 	if( !passMuonSelection_ZMET_v2(muind, true, true) ) continue;
+    return true;
+  }
+  return false;
+}
+
+bool overlapElectron_ZMET_v1( int index , float ptcut ){
+  for( unsigned int elind = 0; elind < cms3.els_p4().size(); elind++ ){
+    float dr = ROOT::Math::VectorUtil::DeltaR( cms3.photons_p4().at(index) , cms3.els_p4().at(elind) );    
+    if( dr > 0.2                                          ) continue;
+    if( cms3.els_p4().at(elind).pt() < ptcut              ) continue;
+	if( !passElectronSelection_ZMET_v2(elind, true, true) ) continue;
     return true;
   }
   return false;
@@ -29,7 +39,7 @@ bool passElectronSelection_ZMET(int index ){
 }
 
 bool passElectronSelection_ZMET_v2(int index, bool vetoTransition, bool eta24 ){
-  if( fabs(cms3.els_p4().at(index).pt()) < 15.0    ) return false; // pT > 15 GeV - Minimum pT cut
+  if( fabs(cms3.els_p4().at(index).pt()) < 10.0    ) return false; // pT > 15 GeV - Minimum pT cut
   if( vetoTransition
 	  && fabs(cms3.els_etaSC().at(index)) > 1.4442
 	  && fabs(cms3.els_etaSC().at(index)) < 1.566  ) return false; // veto x-ition region
@@ -85,7 +95,7 @@ bool passMuonSelection_ZMET(int index ){
 }
 
 bool passMuonSelection_ZMET_v2(int index, bool vetoTransition, bool eta24 ){
-  if( fabs(cms3.mus_p4().at(index).pt()) < 15.0       ) return false; // pT > 10 GeV - Minimum pT cut
+  if( fabs(cms3.mus_p4().at(index).pt()) < 10.0       ) return false; // pT > 10 GeV - Minimum pT cut
   if( vetoTransition
 	  && fabs(cms3.mus_p4().at(index).eta()) > 1.4442
 	  && fabs(cms3.mus_p4().at(index).eta()) < 1.566  ) return false; // veto x-ition region
@@ -125,6 +135,22 @@ bool passMuonSelection_ZMET_v1(int index, bool vetoTransition, bool eta24 ){
   if( eta24
 	  && fabs(cms3.mus_p4().at(index).eta()) > 2.4    ) return false; // eta < 2.4
   if( !muonID( index, ZMET_tight_v1 )                 ) return false; // tight Muon ID  
+  return true;
+}
+
+bool passPhotonSelection_ZMET(int index ){
+  return passPhotonSelection_ZMET_v2(index, true, true);
+}
+
+  bool passPhotonSelection_ZMET_v2(int index, bool vetoTransition, bool eta24 ){
+  if( fabs(cms3.photons_p4().at(index).pt()) < 22.0       ) return false; // pT > 22 GeV - Minimum pT cut
+  if( vetoTransition
+	  && fabs(cms3.photons_p4().at(index).eta()) > 1.4442
+	  && fabs(cms3.photons_p4().at(index).eta()) < 1.566  ) return false; // veto x-ition region
+  if( eta24
+	  && fabs(cms3.photons_p4().at(index).eta()) > 2.4    ) return false; // eta < 2.4
+  if( overlapElectron_ZMET_v1( index, 10.0 )              ) return false; // remove electrons from W
+  if( !photonID(index, ZMET_photon_v1 )                   ) return false;
   return true;
 }
 
--- OSSelections.h
+++ OSSelections.h
@@ -4,7 +4,8 @@
 #ifndef OSSELECTIONS_H
 #define OSSELECTIONS_H
 
-bool overlapMuon_ZMET_v1( int index , float ptcut = 10.0 );
+bool overlapMuon_ZMET_v1    ( int index , float ptcut = 10.0 );
+bool overlapElectron_ZMET_v1( int index , float ptcut = 10.0);
 
 bool passElectronSelection_ZMET(int index );
 bool passElectronSelection_ZMET_v2(       int index, bool vetoTransition, bool eta24 );
@@ -18,6 +19,8 @@ bool passMuonSelection_ZMET_NoIso_v2( int index, bool vetoTransition, bool eta24
 bool passMuonSelection_ZMET_v1_NoIso( int index, bool vetoTransition, bool eta24 );
 bool passMuonSelection_ZMET_v1(       int index, bool vetoTransition, bool eta24 );
 
+bool passPhotonSelection_ZMET(int index );
+bool passPhotonSelection_ZMET_v2(int index, bool vetoTransition, bool eta24 );
 bool passPhotonSelection_ZMET_v1(int index, bool vetoTransition, bool eta24 );
 
 #endif
