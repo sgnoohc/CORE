826c715a0ef07b24663ed2a919b4efd77f0d2de3
gzevi <giovanni.zevidellaporta@gmail.com>
2015-10-14 08:04:15 -0700

    added spring15 50ns id for MT2 (HAD 3)

--- PhotonSelections.cc
+++ PhotonSelections.cc
@@ -41,6 +41,21 @@ bool isLoosePhoton(unsigned int phIdx, analysis_t analysis, int version){
       } 
       else return false;
     } 
+    if (version == 3){ // Based on 50ns Spring15 IDs
+      if( fabs(photons_p4().at(phIdx).eta()) <= 1.479 ){
+        if(photons_full5x5_sigmaIEtaIEta().at(phIdx) >= 0.015) return false; // looser than POG (0.01) to get a sideband
+        if(photons_full5x5_hOverEtowBC().at(phIdx) >= 0.05) return false; 
+        if(photons_haspixelSeed().at(phIdx)) return false;
+        return true;
+      } 
+      else if( fabs(photons_p4().at(phIdx).eta()) > 1.479 && fabs(photons_p4().at(phIdx).eta())  < 2.5){
+        if(photons_full5x5_sigmaIEtaIEta().at(phIdx) >= 0.035) return false; // larger than POG (0.0321) to get a sideband
+        if(photons_full5x5_hOverEtowBC().at(phIdx) >= 0.05) return false; 
+        if(photons_haspixelSeed().at(phIdx)) return false;
+        return true;
+      } 
+      else return false;
+    } 
   }
   else {
     std::cerr << "CORE::PhotonSelection -- Photon selection not implemented for analysis " << analysis << std::endl;
@@ -74,6 +89,18 @@ bool isTightPhoton(unsigned int phIdx, analysis_t analysis, int version){
       } 
       else if( fabs(photons_p4().at(phIdx).eta()) > 1.479 && fabs(photons_p4().at(phIdx).eta())  < 2.5){
         if(photons_full5x5_sigmaIEtaIEta().at(phIdx) >= 0.0266) return false; 
+        return true;
+      } 
+      else return false;
+    } 
+    if (version == 3){
+      if (!isLoosePhoton(phIdx, HAD, 3)) return false;
+      if( fabs(photons_p4().at(phIdx).eta()) <= 1.479 ){
+        if(photons_full5x5_sigmaIEtaIEta().at(phIdx) >= 0.0103) return false; 
+        return true;
+      } 
+      else if( fabs(photons_p4().at(phIdx).eta()) > 1.479 && fabs(photons_p4().at(phIdx).eta())  < 2.5){
+        if(photons_full5x5_sigmaIEtaIEta().at(phIdx) >= 0.0277) return false; 
         return true;
       } 
       else return false;
