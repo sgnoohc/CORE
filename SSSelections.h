dc81dd8d5ef44314fb1d0859a4c3fddcdf098868
Christopher A. George <george@physics.ucsb.edu>
2015-10-29 13:35:07 -0700

    add this

--- SSSelections.h
+++ SSSelections.h
@@ -61,7 +61,7 @@ std::vector<particle_t> getGenPair(bool verbose=false);
 
 //Signal region selections
 anal_type_t analysisCategory(int id1, int id2, float lep1pt, float lep2pt);
-int baselineRegion(int njets, int nbtags, float met, float ht, float lep1_pt, float lep2_pt);
+int baselineRegion(int njets, int nbtags, float met, float ht, int id1, int id2, float lep1_pt, float lep2_pt);
 int signalRegion(int njets, int nbtags, float met, float ht, float mt_min, int id1, int id2, float lep1pt, float lep2pt);
 
 //More Lepton selections
