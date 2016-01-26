b94cff2449f1e20ffdf01f64259db8b481a2e40a
Nick Amin <amin.nj@gmail.com>
2016-01-26 14:26:42 -0800

    sorry made a mistake with the tau commit

--- SSSelections.cc
+++ SSSelections.cc
@@ -778,8 +778,8 @@ pair <int, int> lepMotherID_v2(Lep lep){
   //Now we are matched, classify it
   if ((abs(id) != abs(id_reco)) && abs(id) != 22) return make_pair(0, idx); 
   if (tas::evt_isRealData()) return make_pair(1, idx);
-  if (abs(id) == 22 || tas::genps_isDirectPromptTauDecayProductFinalState().at(idx) || (abs(id) == abs(id_reco) && abs(mother_id) == 22)){
-    if (tas::genps_isPromptFinalState().at(idx) || tas::genps_isHardProcess().at(idx)) return make_pair(-3, idx);
+  if (abs(id) == 22  || (abs(id) == abs(id_reco) && abs(mother_id) == 22)){
+    if (tas::genps_isPromptFinalState().at(idx) || tas::genps_isDirectPromptTauDecayProductFinalState().at(idx) || tas::genps_isHardProcess().at(idx)) return make_pair(-3, idx);
     else return make_pair(0, idx); 
   }
   //If you get here, we have a well-matched MC particle.  Now check its pedigree.
