1698a14ffb351c737e2638c8c667cac110eabdc5
olivito <dominick.olivito@gmail.com>
2017-02-02 02:30:18 -0800

    updated header file

--- OSSelections.h
+++ OSSelections.h
@@ -8,6 +8,7 @@ bool overlapMuon_ZMET_v1    ( int index , float ptcut = 10.0 );
 bool overlapElectron_ZMET_v1( int index , float ptcut = 10.0);
 
 bool passElectronSelection_ZMET(int index );
+bool passElectronSelection_ZMET_v6(            int index, bool vetoTransition, bool eta24 );
 bool passElectronSelection_ZMET_v5(            int index, bool vetoTransition, bool eta24 );
 bool passElectronSelection_ZMET_v4(            int index, bool vetoTransition, bool eta24 );
 bool passElectronSelection_ZMET_v3(            int index, bool vetoTransition, bool eta24 );
