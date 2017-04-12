55ec1aeac1aa2b4d203bf03ecfef8f6e32116701
wsicheng <sicheng@physics.ucsb.edu>
2017-04-12 00:05:57 -0700

    Adding helper class to get scale1fb from text file

---
+++ Tools/datasetinfo/getDatasetInfo.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "getDatasetInfo.h"
+
+using namespace std;
+
+void DatasetInfoFromFile::loadFromFile(const string filename) {
+  ifstream ifile(filename);
+  if (!ifile) throw std::invalid_argument("Dataset info file" + filename + "does not exist!");
+  string dsname, cmstag;
+  datasetInfo dsinfo;
+  string line;
+  while (getline(ifile, line)) {
+    if (line.find("# dataset") == 0) {}    // check the ordering of the variables <-- implement later
+    if (line[0] == '#') continue;
+    istringstream lstream(line);
+    string entry;
+    for (int i = 0; lstream >> std::ws >> entry; ++i) {
+      switch (i) {
+      case 0: dsname = entry; break;
+      case 1: cmstag = entry; break;
+      case 2: dsinfo.nevts_tot = stoul(entry); break;
+      case 3: dsinfo.nevts_eff = stoul(entry); break;
+      case 4: dsinfo.xsec = stof(entry); break;
+      case 5: dsinfo.scale1fb = stof(entry); break;
+      case 6: throw std::invalid_argument("Dataset info file has too many arguments! Please update this code!");
+      }
+    }
+    dslist_[cmstag+dsname] = dsinfo; // insert info into the dataset list
+  }
+}
+
+void DatasetInfoFromFile::checkEntryExist(const string dsname, const string cmstag) {
+  if (isEmpty())
+    throw std::invalid_argument("The dataset list is empty. Please check if the dataset info file is properly loaded.");
+  if (dslist_.find(cmstag+dsname) == dslist_.end())
+    throw std::invalid_argument("The dataset " + dsname + " with cmstag: " + cmstag + " cannot be found in the list. Please update the dataset info file.");
+}
+
+float DatasetInfoFromFile::getScale1fbFromFile(const string dsname, const string cmstag) {
+  checkEntryExist(dsname, cmstag);
+  return dslist_[cmstag+dsname].scale1fb;
+}
+
+float DatasetInfoFromFile::getXsecFromFile(const string dsname, const string cmstag) {
+  checkEntryExist(dsname, cmstag);
+  return dslist_[cmstag+dsname].xsec;
+}
+
+unsigned DatasetInfoFromFile::getnEventsTotalFromFile(const string dsname, const string cmstag) {
+  checkEntryExist(dsname, cmstag);
+  return dslist_[cmstag+dsname].nevts_tot;
+}
+
+unsigned DatasetInfoFromFile::getnEventsEffectiveFromFile(const string dsname, const string cmstag) {
+  checkEntryExist(dsname, cmstag);
+  return dslist_[cmstag+dsname].nevts_eff;
+}
---
+++ Tools/datasetinfo/getDatasetInfo.h
@@ -0,0 +1,34 @@
+#ifndef GETDATASETINFO_H
+#define GETDATASETINFO_H
+
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+using namentag = std::pair<string,string>;
+
+class DatasetInfoFromFile {
+ public:
+  DatasetInfoFromFile();
+  DatasetInfoFromFile(const string filename) { loadFromFile(filename); };
+  void loadFromFile(const string filename);
+  void checkEntryExist(const string datasetname, const string cmstag);
+  float getScale1fbFromFile(const string datasetname, const string cmstag);
+  float getXsecFromFile(const string datasetname, const string cmstag);
+  unsigned int getnEventsTotalFromFile(const string datasetname, const string cmstag);
+  unsigned int getnEventsEffectiveFromFile(const string datasetname, const string cmstag);
+  bool isEmpty() { return dslist_.empty(); };
+
+ private:
+  struct datasetInfo {
+    unsigned int nevts_tot;
+    unsigned int nevts_eff;
+    float xsec;
+    float scale1fb;
+    float kfactor;
+    float filtereff;
+  };
+  std::unordered_map<string, datasetInfo> dslist_;
+};
+
+#endif  // GETDATASETINFO_H
