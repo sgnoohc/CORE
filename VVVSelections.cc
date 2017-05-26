e8a7015555055d33ac0cca6e136fa505ff62d164
Bobak Hashemi <bth5032@gmail.com>
2017-05-25 20:51:55 -0700

    updated VVVSelections with veto selections

--- VVVSelections.cc
+++ VVVSelections.cc
@@ -14,6 +14,11 @@ using namespace tas;
 bool passElectronSelection_VVV(int index){
   return passElectronSelection_VVV_v1( index, true, true );
 }
+
+bool passElectronVetoSelection_VVV(int index){
+  return passElectronVetoSelection_VVV_v1( index, true, true );
+}
+
 bool passElectronSelection_VVV_v1(int index, bool vetoTransition, bool eta24){
   if( fabs(cms3.els_p4().at(index).pt()) < 10.0 ) return false; // pT > 10 GeV - Minimum pT cut
   if( vetoTransition && fabs(cms3.els_p4().at(index).eta()) > 1.4 && fabs(cms3.els_p4().at(index).eta()) < 1.6 ) return false; // veto x-ition region
@@ -22,6 +27,15 @@ bool passElectronSelection_VVV_v1(int index, bool vetoTransition, bool eta24){
   return true;
 }
 
+bool passElectronVetoSelection_VVV_v1(int index, bool vetoTransition, bool eta24){
+  if( fabs(cms3.els_p4().at(index).pt()) < 10.0 ) return false; // pT > 10 GeV - Minimum pT cut
+  if( vetoTransition && fabs(cms3.els_p4().at(index).eta()) > 1.4 && fabs(cms3.els_p4().at(index).eta()) < 1.6 ) return false; // veto x-ition region
+  if( eta24 && fabs(cms3.els_p4()[index].eta()) > 2.5 ) return false; // eta < 2.5
+  if( !electronID( index, SS_veto_noiso_v5 ) ) return false; // Electron ID  
+  return true;
+}
+
+
 //~-~-~-~-~-~-~-~//
 //Muon selections//
 //~-~-~-~-~-~-~-~//
@@ -29,10 +43,22 @@ bool passMuonSelection_VVV(int index){
   return passMuonSelection_VVV_v1( index, true, true );
 }
 
+bool passMuonVetoSelection_VVV(int index){
+  return passMuonVetoSelection_VVV_v1( index, true, true );
+}
+
 bool passMuonSelection_VVV_v1(int index, bool vetoTransition, bool eta24){
   if( fabs(cms3.mus_p4().at(index).pt()) < 10.0 ) return false; // pT > 10 GeV - Minimum pT cut
   if( vetoTransition && fabs(cms3.mus_p4().at(index).eta()) > 1.4 && fabs(cms3.mus_p4().at(index).eta()) < 1.6 ) return false; // veto x-ition region
   if( eta24 && fabs(cms3.mus_p4()[index].eta()) > 2.5 ) return false; // eta < 2.5
   if( !muonID( index, VVV_baseline ) ) return false; // Muon ID  
+  return true;
+}
+
+bool passMuonVetoSelection_VVV_v1(int index, bool vetoTransition, bool eta24){
+  if( fabs(cms3.mus_p4().at(index).pt()) < 10.0 ) return false; // pT > 10 GeV - Minimum pT cut
+  if( vetoTransition && fabs(cms3.mus_p4().at(index).eta()) > 1.4 && fabs(cms3.mus_p4().at(index).eta()) < 1.6 ) return false; // veto x-ition region
+  if( eta24 && fabs(cms3.mus_p4()[index].eta()) > 2.5 ) return false; // eta < 2.5
+  if( !muonID( index, SS_veto_noiso_v5 ) ) return false; // Muon ID  
   return true;
 }
--- VVVSelections.h
+++ VVVSelections.h
@@ -6,10 +6,14 @@
 
 
 bool passElectronSelection_VVV(int index);
+bool passElectronVetoSelection_VVV(int index);
 bool passElectronSelection_VVV_v1(int index, bool vetoTransition, bool eta24);
+bool passElectronVetoSelection_VVV_v1(int index, bool vetoTransition, bool eta24);
 
 bool passMuonSelection_VVV(int index);
+bool passMuonVetoSelection_VVV(int index);
 bool passMuonSelection_VVV_v1(int index, bool vetoTransition, bool eta24);
+bool passMuonVetoSelection_VVV_v1(int index, bool vetoTransition, bool eta24);
 
 
 #endif
