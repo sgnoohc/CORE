8d9462e8785e35260dc723ee855a50257bc088ca
wsicheng <sicheng@physics.ucsb.edu>
2017-08-29 16:04:06 -0700

    Partially fix and put deprecate symbols to TrackIso variables

--- IsoTrackVeto.cc
+++ IsoTrackVeto.cc
@@ -3,32 +3,19 @@
 #include "IsolationTools.h"
 #include "CMS3.h"
 
+[[deprecated("The iso variables can be directly accessed in cms4.")]]
 float TrackIso(int thisPf, float coneR, float deltaZCut, bool useFromPV, bool useLeptons){
 
-  float absIso = 0.0;
-  
-  for (int ipf = 0; ipf < (int)cms3.pfcands_p4().size(); ipf++) {
-
-    if( ipf == thisPf ) continue; // skip this PFCandidate
-    if(cms3.pfcands_charge().at(ipf) == 0 ) continue; // skip neutrals
-    double dr=ROOT::Math::VectorUtil::DeltaR( cms3.pfcands_p4().at(ipf) , cms3.pfcands_p4().at(thisPf) );
-    if( dr > coneR ) continue; // skip pfcands outside the cone                                     
-    if( cms3.pfcands_p4().at(ipf).pt()>=0.0 && (useLeptons || abs(cms3.pfcands_particleId().at(ipf)) == 211) && 
-	( (fabs(cms3.pfcands_dz().at(ipf)) < deltaZCut) || (useFromPV && cms3.pfcands_fromPV().at(ipf) > 1)) )
-      absIso += cms3.pfcands_p4().at(ipf).pt();
-
-  }
-
-  return absIso;
-
+  return cms3.pfcands_trackIso().at(thisPf);
 }
 
+[[deprecated("The iso variables can be directly accessed in cms4.")]]
 float MiniTrackIso(int thisPf, float deltaZCut){
-  float pt = cms3.pfcands_p4().at(thisPf).pt();
-  float dr = getMiniDR(pt);
-  return TrackIso(thisPf,dr,deltaZCut);
+
+  return cms3.pfcands_miniTrackIso().at(thisPf);
 }
 
+// Need to add warning to the use of these functions (didn't put in as don't want CORE compile time warning)
 PFCandIsoResults PFCandPFIso(int thisPf, float coneR, float deltaZCut, bool useFromPV, float mindr) {
 
   PFCandIsoResults res;
@@ -57,12 +44,14 @@ PFCandIsoResults PFCandPFIso(int thisPf, float coneR, float deltaZCut, bool useF
   return res;
 }
 
+[[deprecated("This function may not be correct in cms4.")]]
 PFCandIsoResults PFCandMiniPFIso(int thisPf, float deltaZCut){
   float pt = cms3.pfcands_p4().at(thisPf).pt();
   float dr = getMiniDR(pt);
   return PFCandPFIso(thisPf,dr,deltaZCut);
 }
 
+[[deprecated("This function may not be correct in cms4.")]]
 float PFCandRelIsoAn04(int thisPf, bool useDBcor) {
   float pt = cms3.pfcands_p4().at(thisPf).pt();
   PFCandIsoResults results = PFCandPFIso(thisPf, 0.4, 0.0, true, 0.3);
--- IsoTrackVeto.h
+++ IsoTrackVeto.h
@@ -8,10 +8,15 @@ struct PFCandIsoResults {
   float PUpt;
 };
 
+[[deprecated("The iso variables can be directly accessed in cms4.")]]
 float TrackIso(int thisPf, float coneR = 0.3, float deltaZCut = 0.1, bool useFromPV = false, bool useLeptons = true);
+[[deprecated("The iso variables can be directly accessed in cms4.")]]
 float MiniTrackIso(int thisPf, float deltaZCut = 0.1);
+// Need to add warning to the use of these functions (didn't put this in as don't want CORE compile time warning)
 PFCandIsoResults PFCandPFIso(int thisPf, float coneR = 0.3, float deltaZCut = 0.1, bool useFromPV = false, float mindir = -1.);
+[[deprecated("This function may not be correct in cms4.")]]
 PFCandIsoResults PFCandMiniPFIso(int thisPf, float deltaZCut = 0.1);
+[[deprecated("This function may not be correct in cms4.")]]
 float PFCandRelIsoAn04(int thisPf, bool useDBcor = true);
 
 #endif
