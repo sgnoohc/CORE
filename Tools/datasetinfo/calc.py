de1a4002f7f309fa2ba1d3183e904a5ff275cc88
Nick Amin <amin.nj@gmail.com>
2018-02-21 09:15:53 -0800

    turn into function to get line for given metadata, and fix TT

--- Tools/datasetinfo/calc.py
+++ Tools/datasetinfo/calc.py
@@ -4,13 +4,8 @@ import glob
 import sys
 import datetime
 
-sampledirs = glob.glob("/hadoop/cms/store/user/namin/ProjectMetis/*92X*V00-00-06*/")
 
-print "# file created on %s" % (datetime.datetime.now())
-print "# {:153s}  {:17s}  {:9s}  {:9s}  {:8s}  {:10s}".format("dataset","tag","nevts_tot","nevts_eff","xsec","scale1fb")
-for sampledir in sampledirs:
-    metadata = sampledir + "/metadata.json"
-    if not os.path.exists(metadata): continue
+def get_line(metadata):
     with open(metadata,"r") as fhin:
         data = json.load(fhin)
         ijob_to_nevents = data["ijob_to_nevents"]
@@ -19,7 +14,8 @@ for sampledir in sampledirs:
         kfact = data["kfact"]
         xsec = data["xsec"]
         efact = data["efact"]
-        file_indices = map(lambda x: x.rsplit("_",1)[-1].split(".")[0], glob.glob(sampledir+"/*.root"))
+        thedir = metadata.rsplit("/",1)[0]
+        file_indices = map(lambda x: x.rsplit("_",1)[-1].split(".")[0], glob.glob(thedir+"/*.root"))
         nevts_total = 0
         nevts_eff_total = 0
         for fi in file_indices:
@@ -28,6 +24,14 @@ for sampledir in sampledirs:
             nevts_eff_total += nevts_eff
         xsec_total = kfact*xsec*efact
         scale1fb = 1000.0*xsec_total/nevts_eff_total
-        print "{:155s}  {:17s}  {:9}  {:9}  {:8.5g}  {:10.5g}".format(dataset,tag,nevts_total,nevts_eff_total,xsec_total,scale1fb)
+        return "{:155s}  {:17s}  {:9}  {:9}  {:8.5g}  {:10.5g}".format(dataset,tag,nevts_total,nevts_eff_total,xsec_total,scale1fb)
 
+if __name__ == "__main__":
 
+    sampledirs = glob.glob("/hadoop/cms/store/user/namin/ProjectMetis/*92X*V00-00-06*/")
+    print "# file created on %s" % (datetime.datetime.now())
+    print "# {:153s}  {:17s}  {:9s}  {:9s}  {:8s}  {:10s}".format("dataset","tag","nevts_tot","nevts_eff","xsec","scale1fb")
+    for sampledir in sampledirs:
+        metadata = sampledir + "/metadata.json"
+        if not os.path.exists(metadata): continue
+        print get_line(metadata)
